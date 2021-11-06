629909bffc043e5b9d262e7fdb3284060eb013cd
roibar81 <58783670+roibar81@users.noreply.github.com>
2021-11-06 21:43:39 +0200

    Update SameTree.java

--- Easy/SameTree.java
+++ Easy/SameTree.java
@@ -7,8 +7,6 @@ Given the roots of two binary trees p and q, write a function to check if they a
 
 Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
 
- 
-
 Example 1:
 	Input: p = [1,2,3], q = [1,2,3]
 	Output: true
