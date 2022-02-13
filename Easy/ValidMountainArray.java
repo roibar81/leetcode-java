507a760c5ffcce5a51223ce4a7016d76c343eaa0
Roi Barazi <58783670+roibar81@users.noreply.github.com>
2022-02-13 20:05:16 +0200

    Update ValidMountainArray.java

--- Easy/ValidMountainArray.java
+++ Easy/ValidMountainArray.java
@@ -29,23 +29,23 @@ Constraints:
 
 class Solution {
     public boolean validMountainArray(int[] arr) {
-        if(arr.length<3)
+        if(arr.length < 3)
             return false;
         
-        int i=1;
+        int i = 1;
         
-        while(i<arr.length&&arr[i-1]<arr[i]){
+        while(i < arr.length && arr[i - 1] < arr[i]){
             i++;
         }
         
-        if(i==arr.length || i==1)
+        if(i == arr.length || i == 1)
             return false;
         
-        while(i<arr.length&&arr[i-1]>arr[i]){
+        while(i < arr.length && arr[i - 1] > arr[i]){
             i++;
         }
         
-        if(i==arr.length)
+        if(i == arr.length)
             return true;
         else
             return false;
